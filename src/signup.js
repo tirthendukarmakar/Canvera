74572868ab96b810f8e4c4f978fb112c1b07c49f
tirthendukarmakar <153216477+tirthendukarmakar@users.noreply.github.com>
2023-12-07 22:33:52 +0530

    Add files via upload

---
+++ src/signup.js
@@ -0,0 +1,8 @@
+function submitForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var dob = document.getElementById('dob').value;
+  
+    alert('Account created! Log In now\nName: ' + name + '\nEmail: ' + email + '\nDate of Birth: ' + dob);
+  }
+  
